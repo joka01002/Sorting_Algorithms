71e2302b600c2d8aa3600b468dd12d21876a4dee
joka01002 <joka01002@utb.vaxjo.se>
2020-02-03 14:26:19 +0100

    Start of program

---
+++ Sorting_Algorithms/Sorting_Algorithms/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Sorting_Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Sort> numberlist = new List<Sort>();
+            Random number = new Random();
+            for (int i = 0; i < 10000; i++)
+            {
+                int num = number.Next(1, 1000001);
+                numberlist.Add(new Sort(num));
+            }
+            
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            stopwatch.Stop();
+        }
+    }
+}
---
+++ Sorting_Algorithms/Sorting_Algorithms/Sort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Algorithms
+{
+    class Sort
+    {
+        private int number;
+
+        public Sort(int n)
+        {
+            number = n;
+        }
+
+        public int Numberlist
+        {
+            set { number = value; }
+            get { return number; }
+        }
+
+        
+    }
+}
