1897abdf4376f24d6f338afb119e7da6d6736deb
Jonatan Kans <joka01002@utb.vaxjo.se>
2020-02-04 19:56:20 +0100

    Fixed bubblesort and made insertionsort.

--- Sorting_Algorithms/Sorting_Algorithms/Program.cs
+++ Sorting_Algorithms/Sorting_Algorithms/Program.cs
@@ -12,47 +12,30 @@ namespace Sorting_Algorithms
     {
         static void Main(string[] args)
         {
-            int swaps = 0;
+            Sort sort = new Sort();
             List<int> numberlist = new List<int>();
             Random number = new Random();
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 10; i++)
             {
                 int num = number.Next(1, 1000001);
                 numberlist.Add(num);
             }
             List<int> bubblesort = new List<int>(numberlist);
-            
-            Stopwatch bubblewatch = new Stopwatch();
-            bubblewatch.Start();
-            for (int i = 0; i < bubblesort.Count - 1; i++)
-            {
-                for (int j = 0; j < bubblesort.Count - 1; j++)
-                {
-                    if (bubblesort[j] > bubblesort[j + 1])
-                    {
-                        int a = bubblesort[j];
-                        int b = bubblesort[j + 1];
-                        Swap(ref a, ref b);
-                        swaps++;
-                    }
-                }
-            }
-            bubblewatch.Stop();
-            TimeSpan tsb = bubblewatch.Elapsed;
-            double bubbletime = tsb.TotalMilliseconds;
-            Console.WriteLine("Bubblesort time: " + bubbletime + " milliseconds");
-            Console.WriteLine("Number of swaps for bubble sort: " + swaps);
+            List<int> insertionsort = new List<int>(numberlist);
+            List<int> mergesort = new List<int>(numberlist);
 
-            
+            double bubbletime = sort.BubbleSort(bubblesort);
+            Console.WriteLine("Bubblesort time: " + bubbletime + " milliseconds");
+
+            double insertiontime = sort.InsertionSort(insertionsort);
+            Console.WriteLine("Insertionsort time: " + insertiontime + " milliseconds");
+
+            double mergetime = sort.MergeSort(mergesort);
+            Console.WriteLine("Mergesort time: " + mergetime + " milliseconds");
+
+
 
             Console.ReadLine();
-        }
-
-        static void Swap(ref int a, ref int b)
-        {
-            int temp = a;
-            a = b;
-            b = temp;
         }
     }
 }
--- Sorting_Algorithms/Sorting_Algorithms/Sort.cs
+++ Sorting_Algorithms/Sorting_Algorithms/Sort.cs
@@ -3,13 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
 
 namespace Sorting_Algorithms
 {
-    class Sort
+    public class Sort
     {
-        
+        public double BubbleSort(List<int> bubblesort)
+        {
+            Stopwatch bubblewatch = new Stopwatch();
+            bubblewatch.Start();
+            for (int i = 0; i < bubblesort.Count - 1; i++)
+            {
+                for (int j = 0; j < bubblesort.Count - 1; j++)
+                {
+                    if (bubblesort[j] > bubblesort[j + 1])
+                    {
+                        int a = bubblesort[j];
+                        bubblesort[j] = bubblesort[j + 1];
+                        bubblesort[j + 1] = a;
+                    }
+                }
+            }
+            bubblewatch.Stop();
+            return bubblewatch.Elapsed.TotalMilliseconds;
+        }
 
-        
+        public double InsertionSort(List<int> insertionsort)
+        {
+            Stopwatch insertionwatch = new Stopwatch();
+            insertionwatch.Start();
+            for (int i = 1; i < insertionsort.Count; i++)
+            {
+                int j = i;
+                while (j > 0 && insertionsort[j-1] > insertionsort[j])
+                {
+                    int a = insertionsort[j-1];
+                    insertionsort[j-1] = insertionsort[j];
+                    insertionsort[j] = a;
+                    j--;
+                }
+            }
+            insertionwatch.Stop();
+            return insertionwatch.Elapsed.TotalMilliseconds;
+        }
+
+        public double MergeSort(List<int> mergesort)
+        {
+            Stopwatch mergewatch = new Stopwatch();
+            mergewatch.Start();
+            MergeSplit(mergesort);
+            mergewatch.Stop();
+            return mergewatch.Elapsed.TotalMilliseconds;
+        }
+
+        private List<int> MergeSplit(List<int> mergesort)
+        {
+
+        }
     }
 }
